a464f135b0e1d2adb1a9c2326343811e35eaf366
fumiya.harada <fumiya.tech@gmail.com>
2020-03-16 08:51:55 +0000

    fix redirect

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   has_many :favorites
-  has_many :favorite_microposts, through: :favorites, source: :micropost
+  has_many :favorite_microposts, through: :favorites, source: :micropost, dependent: :destroy
   validates :content, presence: true, length: { maximum: 255 }
 end
