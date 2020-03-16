6ce9e7058632c0c30ca827c8c132de6fea2113fd
fumiya.harada <fumiya.tech@gmail.com>
2020-03-16 11:54:33 +0000

    fix_reference

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,14 @@
 class FavoritesController < ApplicationController
   before_action :require_user_logged_in
   def create
-    user = User.find(params[:micropost_id])
+    user = Micropost.find(params[:micropost_id])
     current_user.favorite(user)
     flash[:success] = 'お気に入りに登録しました。'
     redirect_to user_path(current_user)
   end
 
   def destroy
-    user = User.find(params[:micropost_id])
+    user = Micropost.find(params[:micropost_id])
     current_user.unfavorite(user)
     flash[:success] = 'お気に入りを外しました。'
     redirect_to user_path(current_user)
